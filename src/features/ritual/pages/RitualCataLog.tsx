594870ca0304e6becb5c8ddefe029e476baf3769
CodeLatBug <153360924+longd139@users.noreply.github.com>
2026-08-06 21:10:12 +0700

    add error

--- src/features/ritual/pages/RitualCataLog.tsx
+++ src/features/ritual/pages/RitualCataLog.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { useRitualCatalog } from "../hooks/useRitualCatalog";
 import { ErrorState } from "@/shared/components/common/ErrorState";
 import { EmptyState } from "@/shared/components/common/EmptyState";
